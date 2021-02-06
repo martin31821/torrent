6c75bad270b09178d2a98637fd92a7c78e1353c2
Clemens Beck <clemens.beck97@gmx.de>
2021-02-06 17:13:59 +0100

    Tracker Support: Parse Peer for Scion HTTP Tracker response

--- tracker/peer.go
+++ tracker/peer.go
@@ -1,24 +1,48 @@
 package tracker
 
 import (
-	"net"
-
+	"fmt"
 	"github.com/anacrolix/dht/v2/krpc"
+	"github.com/scionproto/scion/go/lib/snet"
+	"net"
 )
 
 type Peer struct {
-	IP   net.IP
-	Port int
-	ID   []byte
+	IsScionPeer bool
+	udpAddr     *snet.UDPAddr
+	IP          net.IP
+	Port        int
+	ID          []byte
 }
 
 // Set from the non-compact form in BEP 3.
 func (p *Peer) fromDictInterface(d map[string]interface{}) {
+	if _, ok := d["ia"]; ok {
+		p.parseScionPeer(d)
+		return
+	}
+	p.parseDefaultPeer(d)
+}
+
+func (p *Peer) parseDefaultPeer(d map[string]interface{}) {
 	p.IP = net.ParseIP(d["ip"].(string))
 	if _, ok := d["peer id"]; ok {
 		p.ID = []byte(d["peer id"].(string))
 	}
 	p.Port = int(d["port"].(int64))
+	p.IsScionPeer = false
+}
+
+func (p *Peer) parseScionPeer(d map[string]interface{}) {
+	udpStr := fmt.Sprintf("%s,[%s]:%d", d["ia"], d["ip"], d["port"])
+	udpAddr, err := snet.ParseUDPAddr(udpStr)
+	if err != nil {
+		p.IsScionPeer = true
+		p.udpAddr = udpAddr
+	}
+	if _, ok := d["peer id"]; ok {
+		p.ID = []byte(d["peer id"].(string))
+	}
 }
 
 func (p Peer) FromNodeAddr(na krpc.NodeAddr) Peer {
